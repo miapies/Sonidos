c62fe599986ba8ac6a012a7d0fba8e2d074c625b
Jimenez Jimenez <m.jimenez.jimenez@accenture.com>
2017-10-27 13:21:42 +0200

    Subida inicial de aplicación de sonidos

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { ANIMALES } from '../../data/data.animales';
+import { Animal } from '../../interfaces/animal.interface';
+import { Refresher, reorderArray } from 'ionic-angular';
+import { ReorderIndexes } from 'ionic-angular/components/item/item-reorder';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,84 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  animales: Animal[] = [];
+  audio = new Audio();
+  audioTiempo: any;
+  reordenando = false;
+  botonReordenar = 'Ordenar';
+
+  constructor() {
+    this.animales = ANIMALES.slice(0);
+  }
+
+  reproducir(animal: Animal) {
+
+    if (this.reordenando) {
+      return;
+    }
+
+    this.pausar_audio(animal);
+
+    if (animal.reproduciendo) {
+      animal.reproduciendo = false;
+      return;
+    }
+
+    console.log(animal);
+    this.audio.src = animal.audio;
+    this.audio.load();
+    this.audio.play();
+
+    animal.reproduciendo = true;
+
+    this.audioTiempo = setTimeout(() =>
+      animal.reproduciendo = false, animal.duracion * 1000);
+  }
+
+  borrar_animal(index: number) {
+    this.animales.splice(index, 1);
+  }
+
+  recargar_animales(refresher: Refresher) {
+
+    console.log('Inicio del refresh');
+
+    setTimeout(() => {
+      console.log('Fin del refresh');
+      this.animales = ANIMALES.slice(0);
+      refresher.complete();
+    }, 1500);
 
   }
 
+  toggleEdit() {
+
+    this.pausar_audio();
+
+    this.reordenando = !this.reordenando;
+    this.botonReordenar =
+      this.botonReordenar === 'Ordenar' ? 'Listo' : 'Ordenar';
+
+  }
+
+  reordenar_animales(indexes: ReorderIndexes) {
+
+    this.animales = reorderArray(this.animales, indexes);
+  }
+
+  private pausar_audio(animalSel?: Animal) {
+    clearTimeout(this.audioTiempo);
+    this.audio.pause();
+    this.audio.currentTime = 0;
+
+    if (animalSel) {
+      this.animales
+        .filter(animal => animal.nombre != animalSel.nombre)
+        .map(animal => animal.reproduciendo = false);
+    } else {
+      this.animales
+        .map(animal => animal.reproduciendo = false);
+    }
+
+  }
 }
